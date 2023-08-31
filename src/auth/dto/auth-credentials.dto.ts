b29be4fd69dde3495c5130e1d884ae1538dc4aa2
HelloNatalia <nataliatucka01@gmail.com>
2023-08-31 12:23:44 +0200

    Added signup feature

---
+++ src/auth/dto/auth-credentials.dto.ts
@@ -0,0 +1,7 @@
+export class AuthCredentialsDto {
+  username: string;
+  password: string;
+  name: string;
+  surname: string;
+  email: string;
+}
--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,4 +1,13 @@
-import { Repository } from 'typeorm';
+import { EntityRepository, Repository } from 'typeorm';
 import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
-export class UsersRepository extends Repository<User> {}
+@EntityRepository(User)
+export class UsersRepository extends Repository<User> {
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password, name, surname, email } = authCredentialsDto;
+
+    const user = this.create({ username, password, name, surname, email });
+    await this.save(user);
+  }
+}
