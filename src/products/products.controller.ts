ae885dd0e58276dd0fa7adefd3e61a4761ad51ee
HelloNatalia <nataliatucka01@gmail.com>
2023-09-01 14:03:08 +0200

    Added showing existing products with filters

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards, Query } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { Product } from './product.entity';
@@ -6,6 +6,7 @@ import { Role } from 'src/auth/role.enum';
 import { Roles } from 'src/auth/roles.decorator';
 import { AuthGuard } from '@nestjs/passport';
 import { RolesGuard } from 'src/auth/roles.guard';
+import { GetProductsFilterDto } from './dto/get-products-filter.dto';
 
 @Controller('products')
 export class ProductsController {
@@ -16,5 +17,12 @@ export class ProductsController {
   @UseGuards(AuthGuard(), RolesGuard)
   createProduct(@Body() createProductDto: CreateProductDto): Promise<Product> {
     return this.productsService.createProduct(createProductDto);
+  }
+
+  @Get()
+  getProducts(
+    @Query() getProductsFilterDto: GetProductsFilterDto,
+  ): Promise<Product[]> {
+    return this.productsService.getProducts(getProductsFilterDto);
   }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Product } from './product.entity';
 import { Repository } from 'typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
+import { GetProductsFilterDto } from './dto/get-products-filter.dto';
 
 @Injectable()
 export class ProductsService {
@@ -29,5 +30,40 @@ export class ProductsService {
     }
 
     return product;
+  }
+
+  async getProducts(
+    getProductsFilterDto: GetProductsFilterDto,
+  ): Promise<Product[]> {
+    const { search, price_from, price_to } = getProductsFilterDto;
+    const query = this.productsRepository.createQueryBuilder('product');
+    if (search) {
+      query.andWhere(
+        '(LOWER(product.name) LIKE LOWER(:search) OR LOWER(product.description) LIKE LOWER(:search))',
+        { search: `%${search}%` },
+      );
+    }
+    if (price_from) {
+      const parsedPriceFrom = parseFloat(price_from);
+      query.andWhere('CAST(product.price AS DECIMAL) >= :parsedPriceFrom', {
+        parsedPriceFrom,
+      });
+    }
+
+    if (price_to) {
+      const parsedPriceTo = parseFloat(price_to);
+      query.andWhere('CAST(product.price AS DECIMAL) <= :parsedPriceTo', {
+        parsedPriceTo,
+      });
+    }
+
+    try {
+      const products = await query.getMany();
+      return products;
+    } catch (error) {
+      // logger
+      console.log(error.message);
+      throw new InternalServerErrorException('Something went wrong');
+    }
   }
 }
