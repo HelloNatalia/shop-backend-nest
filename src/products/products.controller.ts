2cfe91d31ceedc88323a4d9ebf68b9ae0133e813
HelloNatalia <nataliatucka01@gmail.com>
2023-09-01 13:23:43 +0200

    Added product entity, dto and create product action

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import { Product } from './product.entity';
+import { Role } from 'src/auth/role.enum';
+import { Roles } from 'src/auth/roles.decorator';
+import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from 'src/auth/roles.guard';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  constructor(private productsService: ProductsService) {}
+
+  @Post('create')
+  @Roles(Role.Admin)
+  @UseGuards(AuthGuard(), RolesGuard)
+  createProduct(@Body() createProductDto: CreateProductDto): Promise<Product> {
+    return this.productsService.createProduct(createProductDto);
+  }
+}
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from './product.entity';
+import { Repository } from 'typeorm';
+import { CreateProductDto } from './dto/create-product.dto';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  constructor(
+    @InjectRepository(Product)
+    private productsRepository: Repository<Product>,
+  ) {}
+
+  async createProduct(createProductDto: CreateProductDto): Promise<Product> {
+    const { name, description, price, img } = createProductDto;
+
+    const product = this.productsRepository.create({
+      name,
+      description,
+      price,
+      img,
+    });
+
+    try {
+      await this.productsRepository.save(product);
+    } catch (error) {
+      console.log(error.message);
+      throw new InternalServerErrorException('Something went wrong');
+    }
+
+    return product;
+  }
+}
