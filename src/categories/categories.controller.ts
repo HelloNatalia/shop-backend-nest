1d2d0245fbd36348dbf59f747c567c5dbe5f4fb7
HelloNatalia <nataliatucka01@gmail.com>
2023-09-30 11:17:03 +0200

    Added getting all categories by admin

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { CategoriesService } from './categories.service';
+import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from 'src/auth/roles.guard';
+import { Roles } from 'src/auth/roles.decorator';
+import { Role } from 'src/auth/role.enum';
+import { Category } from './category.entity';
 
 @Controller('categories')
-export class CategoriesController {}
+@UseGuards(AuthGuard('jwt'), RolesGuard)
+@Roles(Role.Admin)
+export class CategoriesController {
+  constructor(private categoriesService: CategoriesService) {}
+
+  @Get()
+  getCategories(): Promise<Category[]> {
+    return this.categoriesService.getCategories();
+  }
+}
