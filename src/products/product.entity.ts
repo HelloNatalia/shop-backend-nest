40557eaf3f66188ad03994094232e9998cc2852a
HelloNatalia <nataliatucka01@gmail.com>
2023-09-30 14:08:44 +0200

    Added cart entity and modified other related entities

--- src/products/product.entity.ts
+++ src/products/product.entity.ts
@@ -1,6 +1,13 @@
 import { Min } from 'class-validator';
+import { Cart } from 'src/cart/cart.entity';
 import { Category } from 'src/categories/category.entity';
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class Product {
@@ -21,4 +28,7 @@ export class Product {
 
   @Column()
   img: string;
+
+  @OneToMany(() => Cart, (cart) => cart.product, { eager: false })
+  cart: Cart;
 }
