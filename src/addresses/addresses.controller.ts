49e64711a8ca57c2c5c3efafc0d59165b193b3d5
HelloNatalia <nataliatucka01@gmail.com>
2023-09-30 17:50:49 +0200

    Added constructors to controller and service

--- src/addresses/addresses.controller.ts
+++ src/addresses/addresses.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Role } from 'src/auth/role.enum';
+import { Roles } from 'src/auth/roles.decorator';
+import { RolesGuard } from 'src/auth/roles.guard';
+import { AddressesService } from './addresses.service';
 
 @Controller('addresses')
-export class AddressesController {}
+@UseGuards(AuthGuard('jwt'), RolesGuard)
+@Roles(Role.Client)
+export class AddressesController {
+  constructor(private addressesService: AddressesService) {}
+}
